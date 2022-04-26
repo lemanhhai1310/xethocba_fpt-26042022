824092bcf5017cf3ba1435a2f7e58fb1ebc3dad1
hailm <hai.lemanh.1310@gmail.com>
2022-04-26 15:40:02 +0700

    feat: footer, thu vien hinh anh, dăng ký trải nghiệm

--- index.php
+++ index.php
@@ -2,4 +2,71 @@
 <?php $body = '' ?>
 <?php require "template-parts/layouts/header.php"; ?>
 
+<!--Tuyển sinh-->
+<div class="uk-section home__tuyensinh" data-src="images/1x/Asset6.png" uk-img>
+    <div class="uk-container">
+        
+    </div>
+</div>
+<!--/Tuyển sinh-->
+
+<!--Trường học trải nghiệm-->
+<div class="home__trainghiemth uk-background-norepeat uk-background-center-center uk-background-cover" data-src="images/1x/bg_trainghiemth.png" uk-img>
+    <div class="uk-container">
+        <div class="uk-flex-middle uk-grid-collapse uk-grid-10-m" uk-grid>
+            <div class="uk-width-expand">
+                <img src="images/1x/img_trainghiemth.png" alt="">
+            </div>
+            <div class="uk-width-1-3@m">
+                <div class="uk-section-xsmall">
+                    <div class="uk-position-relative home__trainghiemth__box">
+                        <div class="home__trainghiemth__card uk-card uk-card-body uk-card-default uk-flex uk-flex-middle">
+                            <div class="uk-width-1-1">
+                                <div class="item__30">
+                                    <h3 class="uk-h3 uk-text-center home__trainghiemth__title">ĐĂNG KÝ TƯ VẤN</h3>
+                                </div>
+                                <div class="item__30">
+                                    <div class="item__15">
+                                        <input class="uk-input home__trainghiemth__input" type="text" placeholder="Họ và tên">
+                                    </div>
+                                    <div class="item__15">
+                                        <input class="uk-input home__trainghiemth__input" type="tel" placeholder="Điện thoại">
+                                    </div>
+                                    <div class="item__15">
+                                        <input class="uk-input home__trainghiemth__input" type="email" placeholder="Email">
+                                    </div>
+                                    <div class="item__15">
+                                        <input class="uk-input home__trainghiemth__input" type="text" placeholder="Tỉnh / Thành phố">
+                                    </div>
+                                </div>
+                                <div class="item__30 uk-text-center">
+                                    <div class="home__trainghiemth__boxBtn uk-position-relative uk-display-inline-block">
+                                        <button class="home__trainghiemth__btn uk-button uk-border-rounded uk-button-default"><span>ĐĂNG KÝ</span></button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--/Trường học trải nghiệm-->
+
+<!--Thư viện, hình ảnh-->
+<div class="slider-container uk-visible@m">
+    <div class="slider-content-wrapper uk-grid-collapse uk-grid-match uk-child-width-auto uk-flex-nowrap" style="width: max-content" uk-grid>
+        <?php for ($i=0;$i<=3;$i++): ?>
+            <div>
+                <img src="images/1x/hinhanh.png" alt="">
+            </div>
+        <?php endfor; ?>
+    </div>
+</div>
+<div class="uk-hidden@m">
+    <img src="images/1x/hinhanh1.png" alt="">
+</div>
+<!--/Thư viện, hình ảnh-->
+
 <?php require "template-parts/layouts/footer.php"; ?>
--- template-parts/layouts/footer.php
+++ template-parts/layouts/footer.php
@@ -1,3 +1,42 @@
+<?php $ratio = 0.9; ?>
+<div class="footer uk-section-xsmall">
+    <div class="uk-container uk-container-small">
+        <div class="uk-flex-middle uk-child-width-auto uk-flex-between uk-grid-5" uk-grid uk-scrollspy="cls: uk-animation-slide-bottom-small; target: > div; delay: 300; repeat: false">
+            <div>
+                <div class="uk-flex-middle uk-grid-5 uk-grid-10-m" uk-grid>
+                    <div class="uk-width-auto">
+                        <a href="" class="uk-icon-button footer__icon" uk-icon="icon: mail; ratio: <?= $ratio ?>"></a>
+                    </div>
+                    <div class="uk-width-expand">
+                        <span class="footer__txt">daihocfpt@fpt.edu.vn</span>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <a href="" class="uk-button footer__btn uk-button-default uk-button-large uk-border-pill">
+                    <div class="uk-flex-middle uk-grid-5 uk-grid-10-m" uk-grid>
+                        <div class="uk-width-auto">
+                            <span class="uk-icon-button footer__icon" uk-icon="icon: receiver; ratio: <?= $ratio ?>"></span>
+                        </div>
+                        <div class="uk-width-expand">
+                            <span class="footer__txt">024/028 73001866</span>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div>
+                <div class="uk-flex-middle uk-grid-5 uk-grid-10-m" uk-grid>
+                    <div class="uk-width-auto">
+                        <a href="" class="uk-icon-button footer__icon" uk-icon="icon: facebook; ratio: <?= $ratio ?>"></a>
+                    </div>
+                    <div class="uk-width-expand">
+                        <span class="footer__txt">FPT University</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 </div>
 <!--/app-->
 </body>
